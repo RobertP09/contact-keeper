026c7f602a58e022ad2c9a530a076a6a4acddb1e
Robert <robgaray9111@yahoo.com>
2020-02-11 00:24:34 -0800

    Initial Setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 5500;
+
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.json('msg: hello')
+});
+
+//Define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contact', require('./routes/contacts'));
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});
