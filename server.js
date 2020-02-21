faaadf7eec2f16878db922a143b58ea38b47dc68
Robert <robgaray9111@yahoo.com>
2020-02-21 01:03:45 -0800

    Set up for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5500;
@@ -10,14 +11,19 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.json('msg: hello')
-});
-
 //Define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in prod
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
